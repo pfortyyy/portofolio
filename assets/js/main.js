59363cc0c84e689b04dab8575bee6722bca854e3
pfortyyy <sihombingpuput31@gmail.com>
2026-06-30 09:41:54 +0700

    perubahan resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: SnapFolio
-* Template URL: https://bootstrapmade.com/snapfolio-bootstrap-portfolio-template/
-* Updated: Jul 21 2025 with Bootstrap v5.3.7
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
